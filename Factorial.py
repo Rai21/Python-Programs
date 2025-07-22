7fa7103774011a527b579589711ca32261c17ce3
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-07-23 01:46:38 +0530

    Factorial

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+def Factorial(num):
+    if num < 1:
+        return "Negativ number is not allowed"
+    result = 1
+    for i in range(1,num +1):
+        result *=i
+    return result
+
+number = int(input("Enter number: "))
+print(f"Factorial of {number} is {Factorial(number)}")
